db33b329fe8025d3b398f4b89986ae7767007818
sinhhn <sinhhn@gmail.com>
2017-04-02 00:54:51 +0900

    Add oop

--- polymorphism/polymorphism.playground/Contents.swift
+++ polymorphism/polymorphism.playground/Contents.swift
@@ -2,3 +2,25 @@
 
 import UIKit
 
+class Shape {
+    var area: Double?
+    func calculateArea(valA: Double, valB: Double) {
+        
+    }
+}
+
+class Rectangle: Shape {
+    override func calculateArea(valA: Double, valB: Double) {
+        area = valA * valB
+    }
+}
+
+class Triangle: Shape {
+    override func calculateArea(valA: Double, valB: Double) {
+        area = (valA * valB) / 2
+    }
+}
+
+func polymorphismPartical(shape: Shape) -> Double? {
+    return shape.area
+}
